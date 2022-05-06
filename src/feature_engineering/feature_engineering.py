9e1f3d7120eb2b482ae7dc29a012f137ae46f63a
Erick Robledo <eriickeduardo97@gmail.com>
2022-05-06 00:13:39 -0500

    Added 2 pipelines, and added paths to the processed data.

--- src/feature_engineering/feature_engineering.py
+++ src/feature_engineering/feature_engineering.py
@@ -1,66 +1,65 @@
 import pandas as pd
-import numpy as np
-from tabnanny import verbose
 from transformers import transformers_categorical as tc
 from transformers import transformers_numerical as tn
-from data.cleaned.utils import config
-from sklearn.preprocessing import OneHotEncoder
+from utils import config
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import MinMaxScaler
 
-train_df = pd.read_csv("data/cleaned/train.csv")
-test_df = pd.read_csv("data/cleaned/test.csv")
+train_df = pd.read_csv(config.TRAIN_DATA_FILE)
+test_df = pd.read_csv(config.TEST_DATA_FILE)
 
-# Categorical variables pipeline
-categorical_imputer_encoder = tc.CategoricalImputerEncoder(config.CATEGORICAL_VARS)
-rare_label_encoder = tc.RareLabelEncoder(variables=["cabin"])
-one_hot_encoder = ColumnTransformer(
-    ["ohe", OneHotEncoder(drop="first"), config.CATEGORICAL_VARS],
-    remainder="passthrough",
-)
-
-categorical_pipe = Pipeline(
+# Numerical and Categorical variables without persisting information - PIPELINE
+vars_without_persisting_info = Pipeline(
     [
-        ("cie", categorical_imputer_encoder),
-        ("rle", rare_label_encoder),
-        ("ohe", one_hot_encoder),
+        ("missing_indicator", tn.Missing_Indicator(config.NUMERICAL_VARS)),
+        ("extract_letter_encoder", tc.ExtractLetterCategoricalEncoder("cabin")),
+        (
+            "categorical_inputer_encoder",
+            tc.CategoricalImputerEncoder(variables=config.CATEGORICAL_VARS),
+        ),
     ]
 )
 
-categorical_pipe.fit(train_df)
+train_df = vars_without_persisting_info.transform(train_df)
+test_df = vars_without_persisting_info.transform(test_df)
 
-processed_train_df = categorical_pipe.transform(train_df)
-processed_test_df = categorical_pipe.transform(test_df)
+# Categorical variables with persisting information
+rare_label_encoder = tc.RareLabelEncoder(tol=0.02, variables=["cabin"])
+rare_label_encoder.fit(train_df)
+train_df = rare_label_encoder.transform(train_df)
+test_df = rare_label_encoder.transform(test_df)
 
-# Numerical variables pipeline
-missing_indicator = tn.Missing_Indicator(config.NUMERICAL_VARS)
+one_hoter_encoder = tc.OneHotEncoderImputer(variables=config.CATEGORICAL_VARS)
+train_df.drop(config.CATEGORICAL_VARS, axis=1, inplace=True)
+test_df.drop(config.CATEGORICAL_VARS, axis=1, inplace=True)
+
+# Numerical variables with persisting information
 numerical_median_imputer = SimpleImputer(strategy="median")
+numerical_median_imputer.fit(train_df[config.NUMERICAL_VARS])
+train_df[config.NUMERICAL_VARS] = numerical_median_imputer.transform(
+    train_df[config.NUMERICAL_VARS]
+)
+test_df[config.NUMERICAL_VARS] = numerical_median_imputer.transform(
+    test_df[config.NUMERICAL_VARS]
+)
+
 min_max_scaler = MinMaxScaler()
 
-numerical_pipe = Pipeline(
-    [
-        ("mi", missing_indicator),
-        ("nmi", numerical_median_imputer),
-        ("mms", min_max_scaler),
-    ]
+# Numerical variables with persisting information - PIPELINE
+numeric_pipeline = Pipeline(
+    [("median_imputer", numerical_median_imputer), ("min_max_scaler", min_max_scaler)]
 )
-
 numeric_col_transformer = ColumnTransformer(
-    [("numeric_transforms", numerical_pipe, config.NUMERICAL_VARS)]
+    [("numeric_transforms", numeric_pipeline, config.NUMERICAL_VARS)]
 )
 
-processed_train_df[config.NUMERICAL_VARS] = numeric_col_transformer.fit_transform(
-    processed_train_df
-)
+train_df[config.NUMERICAL_VARS] = numeric_col_transformer.fit_transform(train_df)
+test_df[config.NUMERICAL_VARS] = numeric_col_transformer.fit_transform(test_df)
 
-processed_test_df[config.NUMERICAL_VARS] = numeric_col_transformer.fit_transform(
-    processed_test_df
-)
-
-processed_test_df = processed_test_df[processed_train_df.columns]
+test_df = test_df[train_df.columns]
 
 # Save processed DFs
-processed_train_df.to_csv(config.PROCESSED_TRAIN_DATA_FILE, index=False)
-processed_test_df.to_csv(config.PROCESSED_TEST_DATA_FILE, index=False)
+train_df.to_csv(config.PROCESSED_TRAIN_DATA_FILE, index=False)
+test_df.to_csv(config.PROCESSED_TEST_DATA_FILE, index=False)
