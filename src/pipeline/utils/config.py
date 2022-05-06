9be2e4aaa19b1f052ed2d9b44b6866d6b9f629bb
Erick Robledo <eriickeduardo97@gmail.com>
2022-05-06 00:21:39 -0500

    Removed, those files and directories weren't necessary.

--- src/pipeline/utils/config.py
+++
@@ -1,7 +0,0 @@
-URL = "https://www.openml.org/data/get_csv/16826755/phpMYEkMl"
-DROP_COLS = ["boat", "body", "home.dest", "ticket", "name"]
-NUMERICAL_VARS = ["pclass", "age", "sibsp", "parch", "fare"]
-CATEGORICAL_VARS = ["sex", "cabin", "embarked", "title"]
-TARGET = "survived"
-SEED_SPLIT = 404
-SEED_MODEL = 404
