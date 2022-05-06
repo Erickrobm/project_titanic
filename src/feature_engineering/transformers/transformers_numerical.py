4a7dcbfea9732decd7918c1ba0f610d3422645ac
Erick Robledo <eriickeduardo97@gmail.com>
2022-05-06 00:06:33 -0500

    Added black format, added pyment numpydoc docstrings to functions and modified many classes.

--- src/feature_engineering/transformers/transformers_numerical.py
+++ src/feature_engineering/transformers/transformers_numerical.py
@@ -5,40 +5,77 @@ from sklearn.impute import SimpleImputer
 
 
 class Missing_Indicator(BaseEstimator, TransformerMixin):
-    def __init__(self, variables:List[str]):
-        self.variables= variables
+    """ """
 
-    def fit(self, X:pd.DataFrame, variables:List[str]):
-        self.variables= variables
-        return self
-
-    def transform(self, X: pd.DataFrame, variables:List[str]):
-        self.variables=variables
-        X = pd.DataFrame(X).copy()
-        X[self.variables+"_nan"] = X[self.variables].isnull().astype(int)
-        return X
-
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, variables: List[str] = None):
+    def __init__(self, variables: List[str]):
         self.variables = variables
 
     def fit(self, X: pd.DataFrame):
-        X = X.drop(self.variables, axis=1, inplace=True)
-        return X
+        """
+
+        Parameters
+        ----------
+        X: pd.DataFrame : X_train or X_test dataframe.
+
+
+        Returns
+        -------
+
+        """
+        return self
 
     def transform(self, X: pd.DataFrame):
-        return self
+        """
+
+        Parameters
+        ----------
+        X: X_train or X_test dataframe.
+
+
+        Returns
+        -------
+        Returns _nan values in columns in database converted as int64.
+        """
+        for var in self.variables:
+            X[var + "_nan"] = X[var].isnull().astype(int)
+        return X
 
 
 class MedianImputer(BaseEstimator, TransformerMixin):
+    """ """
+
     def __init__(self, variables: List[str]):
         self.variables = variables
 
     def fit(self, X: pd.DataFrame, variables: List[str]):
+        """
+
+        Parameters
+        ----------
+        X: X_train or X_test dataframe.
+
+        variables: List[str] : Numerical Variables Array.
+
+
+        Returns
+        -------
+        Database filled with median values instead of nan values.
+        """
         imp_median = SimpleImputer(strategy="median")
         imp_median.fit(X[self.variables])
         X[self.variables] = imp_median.transform(X[self.variables])
         return X
 
     def transform(self, X: pd.DataFrame):
+        """
+
+        Parameters
+        ----------
+        X: pd.DataFrame : X_train or X_test dataframe.
+
+
+        Returns
+        -------
+
+        """
         return self
